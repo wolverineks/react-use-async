12408151486cebec0a2bd4403ac1470c0eef751c
Kevin Sullivan <wolverine.ks@gmail.com>
2019-08-31 06:55:04 -0700

    Add formatting, fix isMounted, fix pending

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'eslint-config-prettier',
+    'prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'eslint-plugin-prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error'
+  }
+}
--- src/useAsync.ts
+++ src/useAsync.ts
@@ -1,89 +1,79 @@
-import * as React from "react";
+import * as React from 'react'
 
 const useIsMounted = () => {
-  const isMounted = React.useRef(false)
-  React.useEffect(() => {
-    isMounted.current = true
-    return () => {
+  const isMounted = React.useRef(true)
+  React.useEffect(
+    () => () => {
       isMounted.current = false
-    }
-  }, [])
+    },
+    []
+  )
 
-  return () => isMounted.current
+  return React.useCallback(() => isMounted.current, [])
 }
 
 interface State {
-  pending: boolean;
-  error?: Error;
-  data?: any;
+  pending: boolean
+  error?: Error
+  data?: any
 }
 
-type Action =
-  | { type: "START" }
-  | { type: "SUCCESS"; data: any }
-  | { type: "ERROR"; error: Error }
-  | { type: "RESET" };
+type Action = { type: 'START' } | { type: 'SUCCESS'; data: any } | { type: 'ERROR'; error: Error } | { type: 'RESET' }
 
 const initialState: State = {
   pending: false,
   error: undefined,
-  data: undefined,
-};
+  data: undefined
+}
 
 const reducer = (state: State, action: Action) => {
   switch (action.type) {
-    case "START": {
-      return { ...state, pending: true, data: undefined, error: undefined };
+    case 'START': {
+      return { ...state, pending: true, data: undefined, error: undefined }
     }
-    case "SUCCESS": {
-      return { ...state, pending: false, data: action.data };
+    case 'SUCCESS': {
+      return { ...state, pending: false, data: action.data }
     }
-    case "ERROR": {
-      return { ...state, pending: false, error: action.error };
+    case 'ERROR': {
+      return { ...state, pending: false, error: action.error }
     }
-    case "RESET": {
-      return initialState;
+    case 'RESET': {
+      return initialState
     }
 
     default: {
-      throw new Error("Invalid action");
+      throw new Error('Invalid action')
     }
   }
-};
+}
 
 export const useAsync = <Data>(): {
-  data?: Data;
-  error?: Error;
-  onError: (error: Error) => void;
-  onStart: () => void;
-  onSuccess: (data: Data) => void;
-  pending: boolean;
-  reset: () => void;
+  data?: Data
+  error?: Error
+  onError: (error: Error) => void
+  onStart: () => void
+  onSuccess: (data: Data) => void
+  pending: boolean
+  reset: () => void
 } => {
   const isMounted = useIsMounted()
-  const [{ data, error, pending }, dispatch] = React.useReducer(
-    reducer,
-    initialState,
-  );
+  const [{ data, error, pending }, dispatch] = React.useReducer(reducer, initialState)
 
   const onStart = React.useCallback(() => {
     if (pending) throw new Error('Multiple async actions. Use "pending" to prevent this.')
-    isMounted() && dispatch({ type: "START" })
-  }, []);
-  const onSuccess = React.useCallback(
-    (data: Data) => isMounted() && dispatch({ type: "SUCCESS", data }),
-    [],
-  );
-  const onError = React.useCallback(
-    (error: Error) => isMounted() && dispatch({ type: "ERROR", error }),
-    [],
-  );
+    isMounted() && dispatch({ type: 'START' })
+  }, [isMounted, pending])
+
+  const onSuccess = React.useCallback((data: Data) => isMounted() && dispatch({ type: 'SUCCESS', data }), [isMounted])
+
+  const onError = React.useCallback((error: Error) => isMounted() && dispatch({ type: 'ERROR', error }), [isMounted])
+
   const reset = React.useCallback(() => {
     if (pending) throw new Error('Multiple async actions. Use "pending" to prevent this.')
-    isMounted() && dispatch({ type: "RESET" })
-  }, []);
+    isMounted() && dispatch({ type: 'RESET' })
+  }, [isMounted, pending])
 
-  return { onStart, onSuccess, onError, reset, data, error, pending };
-};
+  return { onStart, onSuccess, onError, reset, data, error, pending }
+}
 
 export default useAsync
